a17c2d4271ddabd9b4428c13adb4ccfc7a3852db
sridhar <87705817+sridhar4git@users.noreply.github.com>
2023-06-30 23:21:54 -0500

    Added github pages packages for deployment

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Sridhar's Bookmarks Project
+          Sridhar's React Project
         </p>
       </header>
     </div>
